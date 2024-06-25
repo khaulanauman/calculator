a5eb3b5c68308331b7f24d9d0b1b71cdee3ae3be
khaulanauman <khaulanauman@gmail.com>
2024-06-26 01:14:32 +0500

    added functionality for all buttons and calculate

--- main.js
+++ main.js
@@ -4,6 +4,11 @@ const buttons = document.querySelectorAll(".click-button");
 var textArea = document.querySelector("#input");
 const clearBtn=document.querySelector("#clearButton");
 const delBtn=document.querySelector("#delButton");
+const equalBtn = document.querySelector("#equalButton");
+var displayValue="";
+//finding the index of operator
+
+
 //calculator functions
 let add=(a,b)=>{
     return a+b;
@@ -22,19 +27,20 @@ let b = Number(prompt("Enter number 2"));
 let operator =prompt("click 1 to add 2 to subtract 3 to multiply and 4 to divide"); */
 let operate = (operator,a,b)=>{
     if (operator=="+"){
-        console.log(add(a,b));
+        return(add(a,b));
      }
      else if(operator=="-"){
-         console.log(subtract(a,b));
+         return(subtract(a,b));
      }
-     else if(operator=="*"){
-         console.log(multiply(a,b));
+     else if(operator=="X"){
+         return(multiply(a,b));
      }
-     else if(operator=="/") {
-         console.log(divide(a,b));
+     else if(operator=="÷") {
+         return(divide(a,b));
      }
      else{
           alert("Please enter a valid choice");
+          return displayValue;
      }
 }
 //when num buttons and functions are clicked
@@ -43,22 +49,48 @@ buttons.forEach((button) => {
         console.log("i am clicked");
         var buttonText=button.textContent;
         console.log(buttonText);
-        textArea.value+=buttonText;
+        displayValue+=buttonText;
+        textArea.value=displayValue;
     });
 });
 //when clrBtn is clicked
 clearBtn.addEventListener("click",()=>{
     console.log("i have cleared");
-    textArea.value="";
+    displayValue="";
+    textArea.value=displayValue;
 });
 //when dltBtn is clicked
 delBtn.addEventListener("click",()=>{
     console.log("i have deleted");
-    textArea.value=textArea.value.slice(0,-1);
+    //slice(where to start, how many you want to remove)
+    displayValue=displayValue.slice(0,-1);
+    textArea.value=displayValue;
 });
 //when equal button is clicked
-//when operators is clicked
+equalBtn.addEventListener("click",()=>{
+    let index;
+    var operator;
+    for (let i=0;i<displayValue.length;i++){
+    if(displayValue.charAt(i)=='+'||displayValue.charAt(i)=='-'||displayValue.charAt(i)=='X'||displayValue.charAt(i)=='÷'){
+        index=i;
+        operator=displayValue.charAt(i);
+        break;
+        
+    }    
+}
+    let a=parseFloat(displayValue.substring(0,index));
 
+    let b =parseFloat(displayValue.substring(index+1,displayValue.length));
+    console.log("i am a"+a);
+    console.log("i am b"+b);
+    console.log(operator);
+    console.log("i have calculated");
+    displayValue=operate(operator,a,b);
+    textArea.value=displayValue;
+    
+});
+//when operators is clicked
+textArea.value="";
 
 
 
